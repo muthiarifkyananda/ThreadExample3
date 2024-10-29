49b3c283bab2be83145299d75c94a9cfc9e00a89
muthiarifkyananda <muthiarifkyananda@gmail.com>
2024-10-29 22:26:44 +0700

    push.

---
+++ app/src/main/java/muthiaraa/threadexample3/MainActivity.kt
@@ -0,0 +1,25 @@
+package muthiaraa.threadexample3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import muthiaraa.threadexample3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStartThread.setOnClickListener {
+            startThread()
+        }
+    }
+
+    private fun startThread(){
+        val runnable = ExampleRunnable(10);
+        Thread(runnable).start()
+    }
+}
